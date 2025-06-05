b352599930dc0cdbfd6044660b8ce0f6595a51bc
Wilson Wanja <wanjaswilly@gmail.com>
2025-06-05 18:48:48 +0300

    added a page maker for ease of use in creating pages

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,30 @@
 <?php
 
-
 use Slim\App;
 use Slim\Views\Twig;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 return function (App $app) {
+    // Static core routes
     $pages = [
-        '/' => 'home',
+        '/'          => 'home',
         '/developer' => 'developer',
-        '/support' => 'support',
+        '/support'   => 'support',
     ];
 
     foreach ($pages as $route => $template) {
-        $app->get($route, function ($request, $response, $args) use ($template) {
+        $app->get($route, function (Request $request, Response $response) use ($template) {
             $view = Twig::fromRequest($request);
             return $view->render($response, "pages/{$template}.twig");
         });
     }
-    $app->get('/test-500', function ($request, $response) {
+
+    //  AUTO-GENERATED ROUTES - DO NOT REMOVE THIS LINE
+    // $app->get('/example', fn($req, $res) => $this->get(Twig::class)->render($res, 'pages/example.twig'));
+
+    // Test 500 error
+    $app->get('/test-500', function () {
         throw new \Exception("Intentional test error");
     });
 };
-
-
