139a23eb2e19801d74dd01e5f93378a571706aa9
Wilson Wanja <wanjaswilly@gmail.com>
2025-06-05 15:35:04 +0300

    added custom error pages for 404, and 500

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,12 @@
 <?php
-use Slim\Factory\AppFactory;
+
 use Slim\Views\Twig;
+use Slim\Factory\AppFactory;
 use Slim\Views\TwigMiddleware;
+use Slim\Exception\HttpNotFoundException;
+use Slim\Exception\HttpInternalServerErrorException;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
 
 require __DIR__ . '/vendor/autoload.php';
 
@@ -11,5 +16,34 @@ $twig = Twig::create(__DIR__ . '/templates', ['cache' => false]);
 $app->add(TwigMiddleware::create($app, $twig));
 
 (require __DIR__ . '/routes/web.php')($app);
+
+// Existing setup ...
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+// 404 Not Found
+$errorMiddleware->setErrorHandler(HttpNotFoundException::class, function (
+    Request $request,
+    Throwable $exception,
+    bool $displayErrorDetails
+) use ($app) {
+    $view = Twig::fromRequest($request);
+    $response = new \Slim\Psr7\Response();
+    return $view->render($response->withStatus(404), 'errors/404.twig');
+});
+
+// 500 Internal Server Error
+$errorMiddleware->setDefaultErrorHandler(function (
+    Request $request,
+    Throwable $exception,
+    bool $displayErrorDetails,
+    bool $logErrors,
+    bool $logErrorDetails
+) use ($app) {
+    $view = Twig::fromRequest($request);
+    $response = new \Slim\Psr7\Response();
+    return $view->render($response->withStatus(500), 'errors/500.twig');
+});
+
+
 
 return $app;
